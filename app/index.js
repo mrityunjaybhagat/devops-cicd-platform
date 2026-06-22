a24809972cfff86a16bf43fcb55e20e7fcfcede5
mrityunjaybhagat <mrityunjaybhagat@gmail.com>
2026-06-22 07:27:09 +0000

    Add health endpoint for Kubernetes probes

--- app/index.js
+++ app/index.js
@@ -5,7 +5,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("DevOps CI/CD Platform");
 });
-
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "healthy"
+  });
+});
 app.listen(3000, () => {
   console.log("Application running on port 3000");
 });
